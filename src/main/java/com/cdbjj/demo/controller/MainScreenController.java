55c6417b6fe0aa7dccb492a240c23c4d480a316a
Dylan Childers <dylanc95@gmail.com>
2025-09-14 21:12:31 -0500

    Added packages for controllers, entities, and repositories

---
+++ src/main/java/com/cdbjj/demo/controller/MainScreenController.java
@@ -0,0 +1,14 @@
+package com.cdbjj.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainScreenController {
+
+    @GetMapping("/")
+    public String mainPage() {
+
+        return "main";
+    }
+}
